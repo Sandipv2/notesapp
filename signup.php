9dc0d22fb20e327347c6662b963cfce0c5930e53
sandipv2 <sandiipvarma@gmail.com>
2024-04-20 19:53:11 +0530

    added bootstrap js links. ALL Ok here

--- signup.php
+++ signup.php
@@ -10,22 +10,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if ($password != $cpassword) {
         $error = "Passwords din't matched!";
-    }
-    
-    else {
+    } else {
         $sql = "SELECT * FROM users WHERE username = '$username'";
         $result = mysqli_query($conn, $sql);
         $row_num = mysqli_num_rows($result);
 
         if ($row_num > 0) {
             $error = "Username Already Exists!";
-        }
-
-        else {
+        } else {
             $hashed_password = password_hash($password, PASSWORD_DEFAULT);
             $sql = "INSERT INTO users (username, password) VALUES ('$username', '$hashed_password')";
             $result = mysqli_query($conn, $sql);
-            if($result) {
+            if ($result) {
                 $show_msg = "Account created!";
             }
         }
@@ -44,7 +40,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="//cdn.datatables.net/2.0.3/css/dataTables.dataTables.min.css">
 
     <!-- My style -->
@@ -56,9 +53,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <?php require "components/_navbar.php"; ?>
     <?php
     if ($error) {
-    echo "
+        echo "
     <div class='alert alert-danger alert-dismissible fade show' role='alert'>
-        <strong>Error </strong>".$error."
+        <strong>Error </strong>" . $error . "
         <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
             <span aria-hidden='true'>&times;</span>
         </button>
@@ -67,9 +64,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 
     if ($show_msg) {
-    echo "
+        echo "
     <div class='alert alert-success alert-dismissible fade show' role='alert'>
-        <strong>Congratulation </strong>".$show_msg."
+        <strong>Congratulation </strong>" . $show_msg . "
         <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
             <span aria-hidden='true'>&times;</span>
         </button>
@@ -96,6 +93,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <button type="submit" class="btn btn-primary">Create Account</button>
         </form>
     </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+    <script src="//cdn.datatables.net/2.0.3/js/dataTables.min.js"></script>
 </body>
 
 </html>
