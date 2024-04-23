c0389a000b093ba5f8d6c08afc269a9e6719a9f9
sandipv2 <sandiipvarma@gmail.com>
2024-04-23 22:15:17 +0530

    addded _head.php and img/logo.png and set favicon

---
+++ components/_head.php
@@ -0,0 +1,13 @@
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="//cdn.datatables.net/2.0.3/css/dataTables.dataTables.min.css">
+
+<!-- My style -->
+<link rel="stylesheet" href="css/style.css">
+<link rel="icon" href="img/logo.png" type="image/png">
+<title>Notes Bana Lo - SanSoft.</title>
--- index.php
+++ index.php
@@ -5,18 +5,7 @@ session_start();
 <html lang="en">
 
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="//cdn.datatables.net/2.0.3/css/dataTables.dataTables.min.css">
-
-    <!-- My style -->
-    <link rel="stylesheet" href="css/style.css">
-    <title>Notes Bana Lo - SanSoft.</title>
+    <?php require "components/_head.php";?>
     <style>
         .container {
             font-family: consolas;
